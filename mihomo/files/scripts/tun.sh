b177d2de7686efc20f8b76313f69ea47b9af4e1e
actions-user <kenzok8@users.noreply.github.com>
2024-09-08 22:33:35 +0800

    update 2024-09-08 22:33:35

---
+++ mihomo/files/scripts/tun.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+. "$IPKG_INSTROOT/lib/functions.sh"
+. "$IPKG_INSTROOT/etc/mihomo/scripts/constants.sh"
+
+config_load mihomo
+config_get enabled "config" "enabled" 0
+config_get tcp_transparent_proxy_mode "proxy" "tcp_transparent_proxy_mode"
+config_get udp_transparent_proxy_mode "proxy" "udp_transparent_proxy_mode"
+
+if [ "$enabled" == 1 ] && [[ "$tcp_transparent_proxy_mode" == "tun" || "$udp_transparent_proxy_mode" == "tun" ]]; then
+	nft insert rule inet fw4 input iifname "$TUN_DEVICE" counter accept
+	nft insert rule inet fw4 forward oifname "$TUN_DEVICE" counter accept
+	nft insert rule inet fw4 forward iifname "$TUN_DEVICE" counter accept
+fi
+
+exit 0
---
+++ mihomo/files/uci-defaults/migrate.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+. "$IPKG_INSTROOT/etc/mihomo/scripts/constants.sh"
+
+# since 1.8.0
+
+
+# commit
+uci commit mihomo
+
+# exit with 0
+exit 0
